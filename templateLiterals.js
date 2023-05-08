a692cb84ad9441c16cda944c9c0e9db9d00e2ce8
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-08 23:48:11 +0530

    first commit

---
+++ templateLiterals.js
@@ -0,0 +1,7 @@
+//write table of any number using template literals or template strings
+
+//template literals --> use back ticks(``) rather than the quotes(" ") to define a string
+for(let num=1; num<=10;num++){
+    let tableof =12;
+    console.log(`${tableof} * ${num} = ${tableof*num}`);
+}
