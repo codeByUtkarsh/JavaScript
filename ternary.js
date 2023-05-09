113b958bea61e5e64d4ec3bc9500e4a6be7b57dc
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-09 22:59:55 +0530

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,11 @@
+//It is the only javascript operator that takes 3 operands
+//VarName =(condition)? value1:value2
+
+// var age=19;
+// console.log((age>=18)?"you can vote":"you can not vote");
+
+function sum(a,b){
+    return a+b;
+}
+var add =sum(10,20);
+console.log(add);
