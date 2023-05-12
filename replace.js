a9a4da40f91c71085e5a3108689ad1db1e554e76
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-12 20:03:13 +0530

    replace method example

---
+++ replace.js
@@ -0,0 +1,8 @@
+//--> Replaces a specified value with another value in a string 
+//It returns a new string
+//By Default , replace() method replaces only the first match.
+
+// let str = 'I am rahul roy kapoor rahul';
+// let replaceStr = str.replace('rahul','Rahul');
+// console.log(replaceStr);
+
