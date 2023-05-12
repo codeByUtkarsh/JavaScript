a83a9fb844178e5d066434dd96570aad54cbc190
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-12 20:03:47 +0530

    slice method example

---
+++ slice.js
@@ -0,0 +1,13 @@
+// let myTweets = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat ducimus dolores voluptate quis doloremque aspernatur eius tempore delectus architecto temporibus magni perferendis eveniet dolore quas expedita, cum eaque quam, minima laboriosam neque debitis. Tempore dolores voluptatum sed, officia qui, necessitatibus dolore atque optio velit mollitia quibusdam facere, in sapiente excepturi cum odio similique quis repudiandae molestias incidunt libero laudantium repellat sunt? Quas consectetur, numquam mollitia, nesciunt exercitationem, minus recusandae repellendus deleniti optio sed odit? Ex non"
+// let myActualTweets =  myTweets.slice(0,280);
+// console.log(myActualTweets);
+// console.log(myActualTweets.length);
+
+// var str ="Apple, mango ,kiwi";
+// let res = str.substring(-3,5);
+// console.log(res);
+
+let arr= [2,3,4,5,6,8,9];
+let newArr = arr.filter((currElem)=> currElem >2);
+console.log(newArr);
+
