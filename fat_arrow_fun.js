abca52730434949902135dc32c3205b059b951aa
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-15 22:44:44 +0530

    example of arrow function and find() method

---
+++ fat_arrow_fun.js
@@ -0,0 +1,4 @@
+//Fat Arrow funciton --> It behave same as the function .The main difference is that we can write in less lines . 
+//NOTE - WHEN WE FAT ARROW FUNCTION (=>) REMEMBER TO DEFINE IT FIRST BEFORE CALLING THE FUNCTION
+// const sum =() => `The sum of two numbers is = ${(a=10)+(b=80)}`
+// console.log(sum())
