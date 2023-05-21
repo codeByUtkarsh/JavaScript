22b81738502de5291679287fa41cc7470ac5675f
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-21 22:17:20 +0530

    Object Properties

---
+++ obj_prop.js
@@ -0,0 +1,15 @@
+let myName = 'Utkarsh';
+let myAge =21;
+const myBio ={
+    //we can now deal with the dynamic data by just using the [myVar] in place of key
+    // [myName] : 'Hello, How are you?',
+    // [20+6] : 'is my age'
+    
+     // --> No need write key and value, if both are same
+     myName,myAge
+
+}
+console.log(myBio);
+
+
+
