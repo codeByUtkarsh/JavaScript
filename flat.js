3506b31f23bc61a004e88bfe9f8aadfb17fbbcd0
Utkarsh Srivastava <utkarshsrivastava746@gmail.com>
2023-05-22 22:02:54 +0530

    convert one_dimensional array to multidimensional array

---
+++ flat.js
@@ -0,0 +1,8 @@
+// Array.prototype.{flat,flatMap}
+
+//--> It is used to convert one_dimensional array to multidimensional array
+
+const arr =[
+    [1,2],[3,4],[5,6],[7,[8,[9,10]]]
+]
+console.log(arr.flatMap(1));
